a818ca8169619f53d0390244d15468578d95a4d5
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-10-24 15:57:26 -0500

    Add decode method to encodeDecode class

--- encodeDecode.py
+++ encodeDecode.py
@@ -9,3 +9,16 @@ class Solution:
         for s in strs:
             res += str(len(s)) + "#" + s
         return res
+    
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            # find '#'
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            res.append(s[j+1:j+1+length])
+            i = j + 1 + length
+        return res
