dc748be6d5c4a76398058238424fd8bad14b3dc7
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-10-24 15:57:00 -0500

    Implement encode method for string encoding

--- encodeDecode.py
+++ encodeDecode.py
@@ -2,3 +2,10 @@ from typing import List
 
 
 # 271. Encode and Decode Strings
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "#" + s
+        return res
